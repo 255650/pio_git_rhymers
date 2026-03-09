12aadb2e9b6b6c627809afa6c648e8a5f008f120
255650 <255650@edu.p.lodz.pl>
2026-03-09 18:45:36 +0100

    2 zmiana formatowania pliku hanoirythmer przy pomocy tab

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -4,11 +4,10 @@ public class HanoiRhymer extends defaultCountingOutRhymer {
 
 int totalRejected = 0;
 
-    public int reportRejected() {
+public int reportRejected() {
         return totalRejected;
     }
-
-    public void countIn(int in) {
+public void countIn(int in) {
     if (!callCheck() && in > peekaboo())
             totalRejected++;
             else
