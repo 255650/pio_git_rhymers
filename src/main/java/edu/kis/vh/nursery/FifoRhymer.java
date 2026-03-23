e6ed9d29f79848e3e43a3873abc968a71477888e
Jebac <Disa>
2026-03-23 12:40:53 +0100

    9-12. hermetyzacja i poprawa widoczności, getter total, usunięcie setterów

--- src/main/java/edu/kis/vh/nursery/FifoRhymer.java
+++ src/main/java/edu/kis/vh/nursery/FifoRhymer.java
@@ -2,20 +2,18 @@ package edu.kis.vh.nursery;
 
 public class FifoRhymer extends DefaultCountingOutRhymer {
 
-    public final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
+    private  final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
     
     @Override
     public int countOut() {
-        while (!callCheck())
-            
-        temp.countIn(super.countOut());
-        
+        while (!callCheck()) {
+            temp.countIn(super.countOut());
+        }
         int ret = temp.countOut();
         
-        while (!temp.callCheck())
-            
-        countIn(temp.countOut());
-        
+        while (!temp.callCheck()) {
+            countIn(temp.countOut());
+        }
         return ret;
     }
 }
